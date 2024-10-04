b81c5605806115542c940fde9284f4569413dea4
Safuvan <safuvan@adapts.ai>
2024-10-04 21:23:11 +0530

    Add GitAuthEngine's packages from DevCopilot

---
+++ GitAuthEngine/UICDK/GitAuthEngine-cdk/GitAuthEngine-cdk/test/GitAuthEngine-cdk.test.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import cdk_Stack  from '../lib/GitAuthEngine-cdk-stack';
+
+test('SQS Queue and SNS Topic Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new cdk_Stack(app, 'MyTestStack');
+  // THEN
+
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
+  template.resourceCountIs('AWS::SNS::Topic', 1);
+});
+
+//NOTE: replace the cdk_Stack with the name of the class you are exporting from the stack file 
